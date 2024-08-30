81dc448e479cf54ef7c069780172044dbc63423a
Konstantin Yurchenko, Jr <konstantin.k.yurchenko@gmail.com>
2024-08-30 13:12:05 -0700

    Initial Commit

---
+++ Wallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wallet
+//
+//  Created by Konstantin Yurchenko, Jr on 8/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wallet/WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletApp.swift
+//  Wallet
+//
+//  Created by Konstantin Yurchenko, Jr on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
