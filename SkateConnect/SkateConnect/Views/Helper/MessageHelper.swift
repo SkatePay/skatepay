07bb750db71425cc9feab288c331305f24a375fe
Konstantin Yurchenko, Jr. <konstantin.k.yurchenko@gmail.com>
2025-03-21 18:29:29 -0700

    Fixes symbol.

--- SkateConnect/SkateConnect/Views/Helper/MessageHelper.swift
+++ SkateConnect/SkateConnect/Views/Helper/MessageHelper.swift
@@ -357,7 +357,7 @@ extension MessageHelper {
             text: transferTitle,
             attributedText: invoiceAttrText,
             url: invoiceURL,
-            title: "\(invoice.amount) \(symbol)",
+            title: "\(invoice.amount) $\(symbol)",
             teaser: invoice.address,
             thumbnailImage: thumbnailImage ?? image
         )
